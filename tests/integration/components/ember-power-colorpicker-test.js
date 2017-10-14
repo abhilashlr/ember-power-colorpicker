54e8ddfe73681972e00d5c3c9a4c659e89f57aa0
abhilashlr <abhilashlr.789@gmail.com>
2017-10-14 22:21:14 +0530

    Fixed initial test suite

--- tests/integration/components/ember-power-colorpicker-test.js
+++ tests/integration/components/ember-power-colorpicker-test.js
@@ -12,13 +12,4 @@ test('it renders', function(assert) {
   this.render(hbs`{{ember-power-colorpicker}}`);
 
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#ember-power-colorpicker}}
-      template block text
-    {{/ember-power-colorpicker}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
